7641015fbe9f36b88edd5a3b04afaf71ee98a061
vitun7komee <118392333+vitun7komee@users.noreply.github.com>
2024-04-22 22:36:59 +0300

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите пример. Пример: ( ( 1 * 2 ) + ( ( 2 + 3 ) / 1 ) )");
+        System.out.println("Введите пример. Пример: ( ( 3 * 2 ) + ( ( 12 + 4 ) / 2 ) )");
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
         String[] strArr = str.split(" ");
